f3585a959d4a16c49e71e16b5a2a6d7ccf3e31a8
Tom Ogle <togle@foxguardsolutions.com>
2020-08-12 17:13:33 -0400

    Display instructions on startup

--- MarsRover/Program.cs
+++ MarsRover/Program.cs
@@ -1,12 +1,12 @@
-﻿using System;
-
+﻿
 namespace MarsRover
 {
     class Program
     {
         static void Main(string[] args)
         {
-            
+            var client = new RoverClient();
+            client.RoverInstructions();
         }
     }
 }
---
+++ MarsRover/RoverClient.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MarsRover
+{
+    public class RoverClient
+    {
+        public void RoverInstructions()
+        {
+            DisplayInstructions();
+        }
+
+        private void DisplayInstructions()
+        {
+            Console.WriteLine("****Mars Rover Controller****\n");
+            Console.WriteLine("Use the following letters to write a set of commands for the Rover to follow.  Invalid commands will be ignored:");
+            Console.WriteLine("         F = Forward     B = Backward");
+            Console.WriteLine("         L = Turn Left   R = Turn Right");
+            Console.WriteLine("            X = Exit the Controller\n");
+        }
+    }
+}
